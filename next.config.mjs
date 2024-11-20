67374142a56610247930c8d4a097abc80530992e
eazyposy <thakur.raxit@gmail.com>
2024-11-21 00:43:15 +0530

    Fixed Stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-      appDir: true, // If you are using the `/app` directory
-    },
-    api: {
-      bodyParser: false, // Disable body parser for file uploads
-    },
-  };
-  
-  export default nextConfig;
-  
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
