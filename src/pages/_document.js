b4b0c7d34d4e1b44e8319ae983ef042240d49e79
eazyposy <thakur.raxit@gmail.com>
2024-11-21 23:14:14 +0530

    Adsense Bug fixes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,8 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <meta name="google-adsense-account" content="ca-pub-5230983184187192"/>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5230983184187192"
+        <meta name="google-adsense-account" content="ca-pub-5230983184187192"/>
+        <meta name="google-site-verification" content="f08c47fec0942fa0"/>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5230983184187192"
      crossorigin="anonymous"/>
       </Head>
       <body>
