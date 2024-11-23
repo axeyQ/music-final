7ea6fcd23fc9d51f895d1445c8dadb0a47425bf9
eazyposy <thakur.raxit@gmail.com>
2024-11-23 14:51:55 +0530

    Server side issue solve 1

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -6,4 +6,10 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+console.log('Cloudinary Config:', {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET?.slice(0,4) + '...' // Only log first 4 chars for security
+  });
+
 export default cloudinary;
